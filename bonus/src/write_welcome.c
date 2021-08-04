d46d17b7532d31e714c69938ca4afe1d92fc11a8
Ander Amorin sanchez <aamorin-@c3r5s3.42urduliz.com>
2021-08-04 13:36:53 +0200

    :(

---
+++ bonus/src/write_welcome.c
@@ -0,0 +1,8 @@
+Hello welcome!! Write operations to get the numberssorted. For that we have 2 stacks and you can use them as you want.
+The a stack will start with the numbers that you write and b stack for the moment it will be empty.
+Like that!.
+If you write a wrong operation the program return Error.
+To check that the a stack is sort press enter(without anything more). If the numbers are already sorted or you sort with operations the program return OK else KO.
+Write the operation and press enter.
+GL!!!!(Good Luck:)!!!!!)
+Start writing operations
